a1b1db77d781c39ef8995fda9599665b506bf790
Vlad Lysenko <me2ur.cat@yandex.ru>
2024-07-24 12:56:57 +0300

    Адаптация под H2

--- src/main/java/ru/practicum/shareit/item/ItemsRepository.java
+++ src/main/java/ru/practicum/shareit/item/ItemsRepository.java
@@ -12,9 +12,15 @@ public interface ItemsRepository extends JpaRepository<Item, Long> {
 
     Item save(Item item);
 
+    //Для постгрес
+//    @Query(value = "SELECT i.id, i.name, i.description, i.available, i.users_id FROM items AS i " +
+//            "WHERE (UPPER(i.description COLLATE \"en_US\") LIKE UPPER(?1) " +
+//            "OR UPPER(i.name COLLATE \"en_US\") LIKE UPPER(?2)) AND i.available = true", nativeQuery = true)
+//    Set<Item> search(String description, String name);
+
     @Query(value = "SELECT i.id, i.name, i.description, i.available, i.users_id FROM items AS i " +
-            "WHERE (UPPER(i.description COLLATE \"en_US\") LIKE UPPER(?1) " +
-            "OR UPPER(i.name COLLATE \"en_US\") LIKE UPPER(?2)) AND i.available = true", nativeQuery = true)
+            "WHERE (UPPER(i.description) LIKE UPPER(?1) " +
+            "OR UPPER(i.name) LIKE UPPER(?2)) AND i.available = true", nativeQuery = true)
     Set<Item> search(String description, String name);
 
     Item getItemById(Long id);
