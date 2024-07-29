54387e15b539d099bc33351c046f81cd791aca0e
Vlad Lysenko <me2ur.cat@yandex.ru>
2024-07-29 20:04:06 +0300

    Реализация Gateway

---
+++ gateway/src/main/java/ru/practicum/shareit/user/UserClient.java
@@ -0,0 +1,40 @@
+package ru.practicum.shareit.user;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.ResponseExtractor;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+import ru.practicum.shareit.client.BaseClient;
+
+@Component
+public class UserClient extends BaseClient {
+
+    private static final String API_PREFIX = "/users";
+
+    public UserClient(@Value("${shareit-server.url}") String serverUrl, RestTemplateBuilder restBuilder) {
+        super(restBuilder
+                .uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl + API_PREFIX))
+                .requestFactory(() -> new HttpComponentsClientHttpRequestFactory())
+                .build()
+        );
+    }
+
+    public ResponseEntity<Object> add(UserDto dto) {
+        return post("", dto);
+    }
+
+    public ResponseEntity<Object> update(UserDto dto) {
+        return patch("/" + dto.getId(), dto);
+    }
+
+    public ResponseEntity<Object> get(Long userId) {
+        return get("/" + userId);
+    }
+
+    public ResponseEntity<Object> delete(Long userId) {
+        return delete("/" + userId);
+    }
+}
---
+++ gateway/src/main/java/ru/practicum/shareit/user/UserController.java
@@ -0,0 +1,48 @@
+package ru.practicum.shareit.user;
+
+import jakarta.validation.Valid;
+import jakarta.validation.ValidationException;
+import jakarta.validation.constraints.Positive;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserClient userClient;
+
+    @PostMapping
+    public ResponseEntity<Object> add(@Valid @RequestBody UserDto dto) {
+        if (dto.getEmail() == null) {
+            throw new ValidationException("Поле email обязательно для заполнения!");
+        }
+        return userClient.add(dto);
+    }
+
+    @PatchMapping("/{userId}")
+    public ResponseEntity<Object> update(@Valid @RequestBody UserDto userDto,
+                                         @Positive @PathVariable Long userId) {
+        userDto.setId(userId);
+        return userClient.update(userDto);
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<Object> get(@Positive @PathVariable Long userId) {
+        return userClient.get(userId);
+    }
+
+    @DeleteMapping("/{userId}")
+    public void delete(@Positive @PathVariable Long userId) {
+        userClient.delete(userId);
+    }
+
+    @GetMapping
+    public List<UserDto> getAll() {
+        return userService.findAll().stream()
+                .map(userTransfer::toDto)
+                .collect(Collectors.toList());
+    }
+}
