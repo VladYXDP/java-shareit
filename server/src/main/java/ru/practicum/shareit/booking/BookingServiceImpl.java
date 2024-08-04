79b0da8308ef261efb5696ba3243ad5173d8e527
Vlad Lysenko <me2ur.cat@yandex.ru>
2024-08-04 18:25:01 +0300

    Пройдены тесты прошлого спринта

--- server/src/main/java/ru/practicum/shareit/booking/BookingServiceImpl.java
+++ server/src/main/java/ru/practicum/shareit/booking/BookingServiceImpl.java
@@ -139,7 +139,7 @@ public class BookingServiceImpl implements BookingService {
             case CURRENT -> bookings = bookingRepository.findAllByBookerId(userId).stream()
                     .filter(it -> (it.getStatus().equals(BookingStatus.APPROVED) || it.getStatus().equals(BookingStatus.REJECTED))
                             && it.getStartDate().isBefore(now) && it.getEndDate().isAfter(it.getStartDate().plusMinutes(5)))
-                    .sorted(Comparator.comparing(Booking::getStartDate).reversed())
+                    .sorted(Comparator.comparing(Booking::getStartDate))
                     .collect(Collectors.toList());
             case WAITING -> bookings = bookingRepository.findAllByBookerId(userId).stream()
                     .filter(it -> it.getStatus().equals(BookingStatus.WAITING))
